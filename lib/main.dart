80398335bab2004f6168971e89740460a2d193a2
a2c <atsushi1230.sdi@gmail.com>
2022-09-29 23:45:53 +0900

    infinite list view

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
     return MaterialApp(
-        title: 'Welcome to Flutter',
+        title: 'Startup Name Generator',
         home: Scaffold(
             appBar: AppBar(
-              title: const Text('Welcome to Fluter'),
+              title: const Text('Startup Name Generator'),
             ),
             body: const Center(child: RandomWords())));
+  }
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return ListTile(
+          title: Text(_suggestions[index].asPascalCase, style: _biggerFont),
+        );
+      },
+    );
   }
 }
 
@@ -27,12 +50,4 @@ class RandomWords extends StatefulWidget {
 
   @override
   State<RandomWords> createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
-  }
 }
